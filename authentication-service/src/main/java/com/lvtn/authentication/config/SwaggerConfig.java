b979deca40cdb31e5673ebeb87428da17e496160
tdnguyen <tdnguyen16102002@gmail.com>
2024-10-30 16:30:20 +0700

    f

---
+++ authentication-service/src/main/java/com/lvtn/authentication/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.lvtn.authentication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * SwaggerConfig
+ * Version 1.0
+ * Date: 30/10/2024
+ * Copyright
+ * Modification Logs
+ * DATE          AUTHOR          DESCRIPTION
+ * ------------------------------------------------
+ * 30/10/2024        NGUYEN             create
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket docket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.any())
+                .build();
+    }
+}
