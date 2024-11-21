84e802f1eab6e91ed3e2896aa70c5195b4c4d36e
tdnguyen <tdnguyen16102002@gmail.com>
2024-11-21 09:24:33 +0700

    refactor: inventory

---
+++ inventory-service/src/main/java/com/lvtn/inventory/controller/InternalController.java
@@ -0,0 +1,13 @@
+package com.lvtn.inventory.controller;
+/**
+ *InternalController
+ *Version 1.0
+ *Date: 21/11/2024
+ *Copyright
+ *Modification Logs
+ *DATE          AUTHOR          DESCRIPTION
+ *------------------------------------------------
+ *21/11/2024        NGUYEN             create
+ */
+public class InternalController {
+}
---
+++ utils/src/main/java/com/lvtn/utils/dto/inventory/InventoryDto.java
@@ -0,0 +1,13 @@
+package com.lvtn.utils.dto.inventory;
+/**
+ *InventoryDto
+ *Version 1.0
+ *Date: 21/11/2024
+ *Copyright
+ *Modification Logs
+ *DATE          AUTHOR          DESCRIPTION
+ *------------------------------------------------
+ *21/11/2024        NGUYEN             create
+ */
+public class InventoryDto {
+}
